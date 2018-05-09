14eb86d129c944ffcfa273efdf93ffccc7beae24
Doroszlai, Attila <adoroszlai@apache.org>
2018-05-09 18:31:00 +0200

    Revert "AMBARI-23380. Fix intermittent "No such file or directory" error in TestHostCleanup (#1207)"
    This reverts commit 41cd6adcaa9d888ea49fe247f88956f23bcbbb5b.

--- ambari-agent/src/main/python/ambari_agent/HostCleanup.py
+++ ambari-agent/src/main/python/ambari_agent/HostCleanup.py
@@ -425,7 +425,7 @@ class HostCleanup:
     for folder in folders:
       for filename in os.listdir(folder):
         fileToCheck = os.path.join(folder, filename)
-        stat = os.lstat(fileToCheck)
+        stat = os.stat(fileToCheck)
         if stat.st_uid in userIds:
           self.do_erase_dir_silent([fileToCheck])
           logger.info("Deleting file/folder: " + fileToCheck)
