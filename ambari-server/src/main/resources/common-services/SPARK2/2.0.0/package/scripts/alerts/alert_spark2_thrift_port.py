878e2ba52f583587cc84f8aaa8b2665193115ea0
Vitaly Brodetskyi <vbrodetskyi@hortonworks.com>
2018-09-05 00:15:53 +0300

    BUG-110389. STS is showing alerts in ambari part2.(vbrodetskyi)
    Change-Id: Idd9cbbb03fb8a406029b02f6a05628e8acb8b180

--- ambari-server/src/main/resources/common-services/SPARK2/2.0.0/package/scripts/alerts/alert_spark2_thrift_port.py
+++ ambari-server/src/main/resources/common-services/SPARK2/2.0.0/package/scripts/alerts/alert_spark2_thrift_port.py
@@ -131,9 +131,9 @@ def execute(configurations={}, parameters={}, host_name=None):
             host_name = socket.getfqdn()
 
         if security_enabled:
-            beeline_url = ["'jdbc:hive2://{host_name}:{port}/default;principal={hive_principal}'","transportMode={transport_mode}"]
+            beeline_url = ["jdbc:hive2://{host_name}:{port}/default;principal={hive_principal}","transportMode={transport_mode}"]
         else:
-            beeline_url = ["'jdbc:hive2://{host_name}:{port}/default'","transportMode={transport_mode}"]
+            beeline_url = ["jdbc:hive2://{host_name}:{port}/default","transportMode={transport_mode}"]
         # append url according to used transport
 
         beeline_cmd = os.path.join(spark_home, "bin", "beeline")
