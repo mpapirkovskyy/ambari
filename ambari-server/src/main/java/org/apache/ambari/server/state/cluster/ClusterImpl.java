9cf4714cedcdd383e13d01f2889f0acc6aa26745
Myroslav Papirkovskyi <mpapirkovskyy@apache.org>
2019-05-30 16:59:54 +0300

    AMBARI-1240. Initial configuration of services during cluster creation request processing is sequential. (mpapirkovskyy)
    Change-Id: I114bb2be04fae8af617b5aae3a69a446b7492116

--- ambari-server/src/main/java/org/apache/ambari/server/state/cluster/ClusterImpl.java
+++ ambari-server/src/main/java/org/apache/ambari/server/state/cluster/ClusterImpl.java
@@ -1261,6 +1261,20 @@ public class ClusterImpl implements Cluster {
   }
 
   @Override
+  @Transactional
+  public void deleteAllClusterConfigs() {
+    clusterGlobalLock.writeLock().lock();
+    try {
+      Collection<ClusterConfigEntity> clusterConfigs = getClusterEntity().getClusterConfigEntities();
+      for (ClusterConfigEntity clusterConfigEntity : clusterConfigs) {
+        clusterDAO.removeConfig(clusterConfigEntity);
+      }
+    } finally {
+      clusterGlobalLock.writeLock().unlock();
+    }
+  }
+
+  @Override
   public void deleteService(String serviceName)
     throws AmbariException {
     clusterGlobalLock.writeLock().lock();
@@ -1346,6 +1360,7 @@ public class ClusterImpl implements Cluster {
     try {
       refresh();
       deleteAllServices();
+      deleteAllClusterConfigs();
       resetHostVersions();
 
       refresh(); // update one-to-many clusterServiceEntities
@@ -1944,7 +1959,9 @@ public class ClusterImpl implements Cluster {
       }
     }
 
-    clusterEntity = clusterDAO.merge(clusterEntity);
+    for (ClusterConfigEntity clusterConfigEntity : clusterConfigs) {
+      clusterDAO.merge(clusterConfigEntity);
+    }
 
     if (serviceName == null) {
       ArrayList<String> configTypes = new ArrayList<>();
@@ -2428,7 +2445,9 @@ public class ClusterImpl implements Cluster {
 
       // !!! without providing the flush here, when this transaction completes it
       // looks like the database has all unselected configs for some types
-      clusterEntity = clusterDAO.merge(clusterEntity, true);
+      for (ClusterConfigEntity clusterConfig : configEntities) {
+        clusterDAO.merge(clusterConfig, true);
+      }
 
       cacheConfigurations();
 
@@ -2509,7 +2528,9 @@ public class ClusterImpl implements Cluster {
       // since the entities which were modified came from the cluster entity's
       // list to begin with, we can just save them right back - no need for a
       // new collection since the entity instances were modified directly
-      clusterEntity = clusterDAO.merge(clusterEntity, true);
+      for (ClusterConfigEntity clusterConfig : configEntities) {
+        clusterDAO.merge(clusterConfig, true);
+      }
 
       cacheConfigurations();
 
